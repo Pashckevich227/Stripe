f83b5fa8b46504d155d7649a71f4819ccbb14f55
Pashckevich227 <iport234@gmail.com>
2023-02-14 13:21:27 +0300

    feat: basket

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import Buy, create_checkout_session, ShopListView
+from .views import (Buy,
+                    create_checkout_session,
+                    ShopListView,
+                    OrderCreateView,
+                    create_basket)
 
 urlpatterns = [
     path('', ShopListView.as_view()),
     path('item/<int:pk>', Buy.as_view(), name='item'),
     path('buy/<int:pk>', create_checkout_session),
+    path('order/', OrderCreateView.as_view()),
+    path('order/checkout', create_basket)
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, CreateView
 from django.http import HttpResponse
-from .models import Item
+from .models import Item, Order
 import stripe
 import environ
 import os
+from django.core import serializers
 env = environ.Env()
 environ.Env.read_env(os.path.join("/venv/Stripe/", '.env'))
 stripe.api_key = env("STRIPE_API_KEY")
@@ -28,13 +29,31 @@ def create_checkout_session(request, pk):
     )
     return HttpResponse(session.id)
 
-def create_basket(request, pk):
+def create_basket(request):
+    object_list = serializers.serialize("python", Order.objects.all())
+    line_items = []
+
+    for object in object_list:
+        item_data = {
+                'price_data': {
+                    'currency': 'USD',
+                    'product_data': {
+                        'name': 'Telephone',
+                    },
+                    'unit_amount': Item.objects.get(pk=object['fields']['pk_item']),
+                },
+                'quantity': object['fields']['quantity'],
+            }
+        line_items.append(item_data)
+
     session = stripe.checkout.Session.create(
-        line_item=[],
+        line_items = line_items,
         mode='payment',
         success_url='http://localhost:8000/',
-        cancel_url='http://localhost:8000/'
+        cancel_url='http://localhost:8000/',
     )
+    return HttpResponse(session.id)
+
 
 class Buy(DetailView):
     model = Item
@@ -43,3 +62,7 @@ class Buy(DetailView):
 class ShopListView(ListView):
     model = Item
     template_name = 'base.html'
+
+class OrderCreateView(ListView):
+    model = Order
+    template_name = 'order.html'
